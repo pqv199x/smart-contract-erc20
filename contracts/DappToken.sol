706e133f2809651c1d781cb1218750bb39758513
PQV <pqv199x@gmail.com>
2018-06-30 10:20:50 +0700

    Set name, symbol and standard for token

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,13 +1,27 @@
-pragma solidity ^0.4.2;
+pragma solidity ^0.4.4;
 
 contract DappToken {
-  // Contructor
-  // Set the total number of tokens
-  // Read the total number of tokens
   uint256 public totalSupply;
 
-  function DappToken () public {
-    totalSupply = 100000000;
+  // Name
+  string public name = "Test Token";
+  // Symbol
+  string public symbol = "TST";
+  // Standard
+  string public standard = "Test Token v1.0";
 
+  mapping(address => uint256) public balanceOf;
+  
+  /**
+  * Contructor || Read the total number of tokens
+  */
+  function DappToken (uint256 _initialSupply) public {
+    // msg keyword global || sender: the address that call function
+    balanceOf[msg.sender] = _initialSupply;
+    totalSupply = _initialSupply;
   }
+
+  /**
+  * Transfer
+  */
 }
--- test/DappToken.test.js
+++ test/DappToken.test.js
@@ -1,12 +1,32 @@
 const DappToken = artifacts.require('./DappToken.sol');
 
 contract('DappToken', (accounts) => {
-  it('Set the total supply upon deployment', () => {
+  let tokenInstance;
+
+  it('initializes the contract with the correct values', () => {
+    return DappToken.deployed().then((instance) => {
+      tokenInstance = instance;
+      return tokenInstance.name();
+    }).then((name) => {
+      assert.equal(name, 'Test Token', 'has the correct name');
+      return tokenInstance.symbol();
+    }).then((symbol) => {
+      assert.equal(symbol, 'TST', 'has the correct Symbol');
+      return tokenInstance.standard();
+    }).then((standard) => {
+      assert.equal(standard, 'Test Token v1.0', 'has the correct standard');
+    });
+  });
+
+  it('allocates the initial supply upon deployment', () => {
     return DappToken.deployed().then((instance) => {
       tokenInstance = instance;
       return tokenInstance.totalSupply();
     }).then((totalSupply) => {
-      assert.equal(totalSupply.toNumber(), 100000000, 'Sets the total suplly to 1,000,000')
+      assert.equal(totalSupply.toNumber(), 100000000, 'Sets the total suplly to 1,000,000');
+      return tokenInstance.balanceOf(accounts[0]);
+    }).then((adminBalance) => {
+      assert.equal(adminBalance.toNumber(), 100000000, 'allocate the initial supply to the admin account');
     })
   });
 });
